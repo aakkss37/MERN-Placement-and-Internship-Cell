244540d6aa2f645ebcf9c9bdf8231261f78d9ea9
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-13 10:12:04 +0530

    Current page name added at top of the page

--- studentsLogin/src/components/PlavementDrive/PlacementDrive.jsx
+++ studentsLogin/src/components/PlavementDrive/PlacementDrive.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 
+import { Typography } from '@mui/material';
 import MenuBox from '../Constants/UI/menu/MenuBox'
 import Cards from './Card/Card';
 import { DriveContainer, Drives, LeftBoxDummy, MainConatiner } from './PlacementDriveStyle.js';
@@ -9,7 +10,6 @@ import { DriveContainer, Drives, LeftBoxDummy, MainConatiner } from './Placement
 const PlacementDrive = () => {
 	return (
 		<MainConatiner>
-
 			<DriveContainer>
 
 				<MenuBox style={{ boxShadow: '1px 1px 5px 4px rgb(0 0 0 / 30 %)' }} />
@@ -17,9 +17,10 @@ const PlacementDrive = () => {
 				<LeftBoxDummy /> {/* having no content inside it.. just Helping mainBox not to occupy whole area and overlap the Menu */}
 
 				<Drives>
-					<Cards/>
-					<Cards/>
-					<Cards/>
+					<Typography style={{margin: '20px', color: 'gray', fontWeight: 600,}}>PLACEMENT DRIVE</Typography>
+					<Cards />
+					<Cards />
+					<Cards />
 				</Drives>
 
 			</DriveContainer>
