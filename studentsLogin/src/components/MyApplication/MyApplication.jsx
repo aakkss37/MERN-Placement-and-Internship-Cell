698ff11e9d85adfae0641079623349262ae472b7
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-14 01:02:05 +0530

    my application table created

--- studentsLogin/src/components/MyApplication/MyApplication.jsx
+++ studentsLogin/src/components/MyApplication/MyApplication.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
-import { MainConatiner } from './myApplicationStyle.js'
+import { Paper, Table, TableBody, TableContainer, TableHead, TableRow, } from '@mui/material';
+import { CurretPath, MainConatiner, MyApplicationContainer, StyledTableCell, StyledTableRow } from './myApplicationStyle.js'
+
+
+
+const rowsData = [
+	{companyName: 'Google', date: '12 Nov 2022', status: 'Selected'},
+	{companyName: 'Microsoft', date: '11 Oct 2022', status: 'Applied'},
+	{companyName: 'Netflix', date: '12 Dec 2022', status: 'Selected'},
+	{companyName: 'Adobe', date: '27 Nov 2022', status: 'Interview'},
+	{companyName: 'Starlink', date: '17 Dec 2022', status: 'Applied'},
+	{companyName: 'Phonepe', date: '22 Jan 2023', status: 'Interview'},
+	{companyName: 'Google', date: '12 Nov 2022', status: 'Selected'},
+	{companyName: 'Microsoft', date: '11 Oct 2022', status: 'Applied'},
+	{companyName: 'Netflix', date: '12 Dec 2022', status: 'Selected'},
+	{companyName: 'Adobe', date: '27 Nov 2022', status: 'Interview'},
+	{companyName: 'Starlink', date: '17 Dec 2022', status: 'Applied'},
+	{companyName: 'Phonepe', date: '22 Jan 2023', status: 'Interview'},
+	{companyName: 'Google', date: '12 Nov 2022', status: 'Selected'},
+	{companyName: 'Microsoft', date: '11 Oct 2022', status: 'Applied'},
+	{companyName: 'Netflix', date: '12 Dec 2022', status: 'Selected'},
+	{companyName: 'Adobe', date: '27 Nov 2022', status: 'Interview'},
+	{companyName: 'Starlink', date: '17 Dec 2022', status: 'Applied'},
+	{companyName: 'Phonepe', date: '22 Jan 2023', status: 'Interview'},
+];
+
+
 
 const MyApplication = () => {
   return (
-	<MainConatiner>MyApplication</MainConatiner>
+	<MainConatiner>
+		  <MyApplicationContainer>
+
+			  <CurretPath >My Application</CurretPath>
+
+			  <TableContainer component={Paper}>
+				  <Table sx={{ minWidth: 700 }} aria-label="customized table">
+					  <TableHead>
+						  <TableRow >
+							  <StyledTableCell style={{fontWeight: 600}}>S No.</StyledTableCell>
+							  <StyledTableCell style={{fontWeight: 600}}>Company Name</StyledTableCell>
+							  <StyledTableCell align="right" style={{fontWeight: 600}}>Date</StyledTableCell>
+							  <StyledTableCell align="right" style={{fontWeight: 600}}>Status</StyledTableCell>
+						  </TableRow>
+					  </TableHead>
+					  <TableBody>
+						  {rowsData.map((row, index) => (
+							  <StyledTableRow key={row.name}>
+								  <StyledTableCell component="th" scope="row">  {index+1} </StyledTableCell>
+								  <StyledTableCell >{row.companyName}</StyledTableCell>
+								  <StyledTableCell align="right">{row.date}</StyledTableCell>
+								  <StyledTableCell align="right">{row.status}</StyledTableCell>
+							  </StyledTableRow>
+						  ))}
+					  </TableBody>
+				  </Table>
+			  </TableContainer>
+		  </MyApplicationContainer>
+	</MainConatiner>
   )
 }
 
--- studentsLogin/src/components/MyApplication/myApplicationStyle.js
+++ studentsLogin/src/components/MyApplication/myApplicationStyle.js
@@ -1,7 +1,53 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled, TableCell, tableCellClasses, TableRow, Typography } from "@mui/material";
 
 
 
 export const MainConatiner = styled(Box)`
-	margin-top: 75px;
-`
+	margin-top: 85px;
+`;
+
+export const MyApplicationContainer = styled(Box)(({ theme }) => ({
+	width: '75%',
+	margin: '0 auto',
+	// border: '1px solid red',
+	minHeight: 'fit-content',
+	[theme.breakpoints.down('md')]: {
+		width: '98%',
+	}
+}));
+
+
+
+
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+	[`&.${tableCellClasses.head}`]: {
+		backgroundColor: '#cbddc3ad',
+		color: '#367D43',
+	},
+	[`&.${tableCellClasses.body}`]: {
+		fontSize: 14,
+	},
+}));
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+	'&:nth-of-type(odd)': {
+		backgroundColor: 'white',
+	},
+	'&:nth-of-type(even)': {
+		backgroundColor: '#d4e1ce29',
+	},
+	// hide last border
+	'&:last-child td, &:last-child th': {
+		border: 0,
+	},
+}));
+
+
+
+export const CurretPath = styled(Typography)(({ theme }) => ({
+	margin: '20px', color: 'gray', fontWeight: 600,
+	[theme.breakpoints.down('sm')]: {
+		fontSize: 14,
+	}
+}));
