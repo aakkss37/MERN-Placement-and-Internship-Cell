6c65758e107730a5fec78560a272a091f60edc93
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-21 21:56:46 +0530

    styled

--- Global_Interface/src/App.js
+++ Global_Interface/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HiringPartner from './components/HirtingPartners/HiringPartner';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
@@ -7,6 +8,7 @@ function App() {
     <>
 		<Navbar/>
 		<Home/>
+		<HiringPartner/>
 	</>
   );
 }
