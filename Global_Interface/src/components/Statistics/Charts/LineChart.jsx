4c5bdca8cd4d54b3f3458e43440609cf66cdf8c4
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-23 00:42:11 +0530

    bar  chart added

--- Global_Interface/src/components/Statistics/Charts/LineChart.jsx
+++ Global_Interface/src/components/Statistics/Charts/LineChart.jsx
@@ -34,7 +34,24 @@ const LineChartComponent = () => {
 					</AreaChart>
 				</ResponsiveContainer>
 			</div>
-			
+			<div>
+				<h1 className="chart-heading">Bar Chart</h1>
+				<ResponsiveContainer width="100%" aspect={3}>
+					<BarChart
+						data={data}
+
+					>
+						{/* <CartesianGrid strokeDasharray="3 3" /> */}
+						<XAxis dataKey="year" />
+						<YAxis />
+						<Tooltip />
+						<Legend />
+						<Bar dataKey="highest" fill="#8884d8" />
+						<Bar dataKey="lowest" fill="#82ca9d" />
+						<Bar dataKey="average" fill="#FF8A71" />
+					</BarChart>
+				</ResponsiveContainer>
+			</div>
 		</>
 	)
 }
