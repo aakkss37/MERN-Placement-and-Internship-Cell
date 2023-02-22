aa3b4d11ed6305c6af6df5384b435f9b10e850c9
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-22 17:36:29 +0530

    react google chart deleted

--- Global_Interface/src/components/Statistics/Statistic.jsx
+++ Global_Interface/src/components/Statistics/Statistic.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import './statistic.css';
 import CountUp from 'react-countup';
-import Bar from './Charts/Bar';
-import Column from './Charts/Column';
-import Line from './Charts/Line';
-import Pie from './Charts/Pie';
+
 
 const Statistic = () => {
 	return (
-		<div className='statistic'>
+		<div className='statistic' id='statistic'>
 			<h1 className="gradient__text">STATISTICS</h1>
 			<div>
 				<div>
-					<CountUp start={0}
-						end={100}
-						duration={5}
-						useEasing={true}
+					<CountUp start={0} end={100} duration={5} useEasing={true}
 					/>
 				</div>
 				<div>
@@ -28,12 +22,7 @@ const Statistic = () => {
 					<CountUp start={0} end={321} duration={5} useEasing={true}/>
 				</div>
 			</div>
-			<div>
-				<div><Bar/></div>
-				<div><Column /></div>
-				<div><Line /></div>
-				<div><Pie /></div>
-			</div>
+			
 		</div>
 	)
 }
