c407d600e1d321740e7abe302254960bb9aa78bd
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-22 17:50:06 +0530

    count start only when scroll to statistic viewport

--- Global_Interface/src/components/Statistics/Statistic.jsx
+++ Global_Interface/src/components/Statistics/Statistic.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './statistic.css';
 import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
+
 
 
 const Statistic = () => {
+	const [counterOn, setcounterOn] = useState(false);
+
+
+
 	return (
 		<div className='statistic' id='statistic'>
 			<h1 className="gradient__text">STATISTICS</h1>
 			<div>
-				<div>
-					<CountUp start={0} end={100} duration={5} useEasing={true}
-					/>
-				</div>
-				<div>
-					<CountUp start={0} end={321} duration={5} useEasing={true} />
-				</div>
-				<div>
-					<CountUp start={0} end={321} duration={5} useEasing={true}/>
-				</div>
-				<div>
-					<CountUp start={0} end={321} duration={5} useEasing={true}/>
-				</div>
+				<ScrollTrigger onEnter={() => setcounterOn(true)} onExit={() => setcounterOn(false)}>
+					<div>
+						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					</div>
+					<div>
+						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					</div>
+					<div>
+						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					</div>
+					<div>
+						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					</div>
+				</ScrollTrigger>
 			</div>
-			
+
 		</div>
 	)
 }
