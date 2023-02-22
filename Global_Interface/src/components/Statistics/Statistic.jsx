94f99dfb2b062319e4851eb8a533c671be6c2238
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-22 18:44:07 +0530

    counter styled

--- Global_Interface/src/components/Statistics/Statistic.jsx
+++ Global_Interface/src/components/Statistics/Statistic.jsx
@@ -13,22 +13,26 @@ const Statistic = () => {
 	return (
 		<div className='statistic' id='statistic'>
 			<h1 className="gradient__text">STATISTICS</h1>
-			<div>
-				<ScrollTrigger onEnter={() => setcounterOn(true)} onExit={() => setcounterOn(false)}>
-					<div>
-						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+			<ScrollTrigger onEnter={() => setcounterOn(true)} onExit={() => setcounterOn(false)}>
+				<div className='statistic_counter'>
+					<div className='counter_box'>
+						{counterOn && <CountUp start={0} end={320} duration={4} useEasing={true} />}<span>+</span>
+						<p>Companies Chose KAHE students</p>
 					</div>
-					<div>
-						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					<div className='counter_box'>
+						{counterOn && <CountUp start={0} end={150} duration={4} useEasing={true} />}<span>+</span>
+						<p>Companies offering CTC 5 Lacs p.a. or above</p>
 					</div>
-					<div>
-						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					<div className='counter_box'>
+						{counterOn && <CountUp start={0} end={750} duration={4} useEasing={true} />}<span>+</span>
+						<p>Numbar of Offers(p.a) for KAHE Students</p>
 					</div>
-					<div>
-						{counterOn && <CountUp start={0} end={100} duration={4} useEasing={true} />}
+					<div className='counter_box'>
+						<span>₹</span>{counterOn && <CountUp start={0} end={34} duration={4} useEasing={true} />}
+						<p>Heighest CTC <b>(Lakh p.a)</b></p>
 					</div>
-				</ScrollTrigger>
-			</div>
+				</div>
+			</ScrollTrigger>
 
 		</div>
 	)
