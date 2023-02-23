e2b323dea814f52427128e46b4ac42f530a521b7
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-23 14:10:12 +0530

    Bar chart completed

--- Global_Interface/src/components/Statistics/Charts/BarChart.jsx
+++ Global_Interface/src/components/Statistics/Charts/BarChart.jsx
@@ -5,25 +5,25 @@ import { data } from './chartData'
 const BarChartComponent = () => {
   return (
 	  <div className='graph_container'>
-		  <h2 className="chart-heading">Bar Chart</h2>
-		  <ResponsiveContainer width="100%" height='90%'>
+		  <h2 className="chart-heading">Heighest, Lowest & Average CTC (Lacs p.a)</h2>
+		  <ResponsiveContainer width="100%" height='80%'>
 			  <BarChart
 				  data={data}
 				  margin={{
 					  top: 5,
 					  right: 20,
-					  left: -30,
+					  left: -20,
 					  bottom: 0,
 				  }}
 			  >
 				  {/* <CartesianGrid strokeDasharray="3 3" /> */}
 				  <XAxis dataKey="year" />
 				  <YAxis />
-				  <Tooltip />
+				  <Tooltip contentStyle={{ background: '#FF8A71', border: 'none', borderRadius: '3px', fontSize: 12, fontWeight: 600, }} />
 				  <Legend  />
-				  <Bar dataKey="max" fill="#8884d8" />
-				  <Bar dataKey="min" fill="#82ca9d" />
-				  <Bar dataKey="avg" fill="#FF8A71" />
+				  <Bar dataKey="Lowest" fill="red" />
+				  <Bar dataKey="Heighest" fill="green" />
+				  <Bar dataKey="Avrage" fill="steelblue" />
 			  </BarChart>
 		  </ResponsiveContainer>
 	  </div>
