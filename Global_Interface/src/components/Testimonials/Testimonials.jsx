1a0f872914bc2ac748cbe2c196001715228203dd
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-23 17:57:53 +0530

    join us bar styled full responsive

--- Global_Interface/src/components/Testimonials/Testimonials.jsx
+++ Global_Interface/src/components/Testimonials/Testimonials.jsx
@@ -4,16 +4,19 @@ import './testimonials.css'
 const Testimonials = () => {
 	return (
 		<div className='testimonials section__padding' id='testimonials'>
+
 			<div className='join_us_bar'>
 				<div className="join_us_text">
 					<h2>Hey Listen! </h2>
-					<p>Isn't It grate! Our students are doing tremendously nice in there field.</p>
+					<p>Isn't It grate! Our students are doing tremendously nice in there field.</p><br/>
 					<span><b>Come, Join our community of successful students! Reach out to us and learn more about how we're shaping the next generation of leaders.</b></span>
 				</div>
 				<div className="join_us_button">
 					<a href='#home' >Join Us</a>
 				</div>
 			</div>
+
+
 		</div>
 	)
 }
