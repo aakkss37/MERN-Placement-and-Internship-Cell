c94ab4cdd0167e327c701f0435c75acd0101ffbd
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-23 17:15:37 +0530

    scroll trigger removed from graph

--- Global_Interface/src/components/Statistics/Statistic.jsx
+++ Global_Interface/src/components/Statistics/Statistic.jsx
@@ -12,47 +12,43 @@ import PieChartComponent from './Charts/PieChart';
 
 
 const Statistic = () => {
-	const [animationOn, setAnimationOn] = useState(false);
+	const [countOn, setCountOn] = useState(false);
 
 
 
 	return (
 		<div className='statistic' id='statistic'>
-			
+
 			<h1 className="gradient__text">STATISTICS</h1>
-			<ScrollTrigger onEnter={() => setAnimationOn(true)} onExit={() => setAnimationOn(false)}>
+			<ScrollTrigger onEnter={() => setCountOn(true)} onExit={() => setCountOn(false)}>
 				<div className='statistic_counter'>
 					<div className='counter_box'>
-						{animationOn && <CountUp start={0} end={320} duration={4} useEasing={true} />}<span>+</span>
+						{countOn && <CountUp start={0} end={320} duration={4} useEasing={true} />}<span>+</span>
 						<p>Companies Chose KAHE students</p>
 					</div>
 					<div className='counter_box'>
-						{animationOn && <CountUp start={0} end={150} duration={4} useEasing={true} />}<span>+</span>
+						{countOn && <CountUp start={0} end={150} duration={4} useEasing={true} />}<span>+</span>
 						<p>Companies offering CTC 5 Lacs p.a. or above</p>
 					</div>
 					<div className='counter_box'>
-						{animationOn && <CountUp start={0} end={750} duration={4} useEasing={true} />}<span>+</span>
+						{countOn && <CountUp start={0} end={750} duration={4} useEasing={true} />}<span>+</span>
 						<p>Numbar of Offers(p.a) for KAHE Students</p>
 					</div>
 					<div className='counter_box'>
-						<span>₹</span>{animationOn && <CountUp start={0} end={34} duration={4} useEasing={true} />}
+						<span>₹</span>{countOn && <CountUp start={0} end={34} duration={4} useEasing={true} />}
 						<p>Heighest CTC <b>(Lakh p.a)</b></p>
 					</div>
 				</div>
-
-
-				{
-					animationOn &&
-					<div className='statistic_graph'>
-							<LineChartComponent/>
-							<AreaChartComponent/>
-							<BarChartComponent/>
-							<PieChartComponent/>
-					</div>
-				}
-
-
 			</ScrollTrigger>
+
+
+			<div className='statistic_graph' >
+				<LineChartComponent />
+				<AreaChartComponent />
+				<BarChartComponent />
+				<PieChartComponent />
+			</div>
+
 		</div>
 	)
 }
