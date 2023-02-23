7bd471066c24d3f19c66c3b960e99a230ce17be0
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-23 16:28:17 +0530

    scroll to top button added

--- Global_Interface/src/App.js
+++ Global_Interface/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ScrollToTop from "react-scroll-to-top";
 import HiringPartner from './components/HirtingPartners/HiringPartner';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
@@ -11,6 +12,15 @@ function App() {
 		<Home/>
 		<HiringPartner/>
 		<Statistic/>
+
+		
+		
+		  <ScrollToTop 
+		  	smooth 
+			color="#fff" 
+			style={{ background: '#ff4820cf', color: 'white'}}  
+		/>
+		
 	</>
   );
 }
