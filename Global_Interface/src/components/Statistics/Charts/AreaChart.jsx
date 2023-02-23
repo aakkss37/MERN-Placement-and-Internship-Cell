158d06a8cd003f0b58049c6ff8dab012a02c3810
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-23 13:54:32 +0530

    Area chart completed

--- Global_Interface/src/components/Statistics/Charts/AreaChart.jsx
+++ Global_Interface/src/components/Statistics/Charts/AreaChart.jsx
@@ -5,19 +5,19 @@ import { data } from './chartData'
 const AreaChartComponent = () => {
   return (
 	  <div className='graph_container'>
-		  <h2 className="chart-heading">Area Chart</h2>
-		  <ResponsiveContainer width="100%" height='90%'>
+		  <h2 className="chart-heading">Total Recruiters at KAHE</h2>
+		  <ResponsiveContainer width="100%" height='80%'>
 			  <AreaChart data={data} margin={{
 				  top: 5,
 				  right: 20,
-				  left: -30,
+				  left: -20,
 				  bottom: 0,
 			  }}>
 				  <XAxis dataKey="year" />
 				  <YAxis />
-				  <Tooltip />
+				  <Tooltip contentStyle={{ background: '#FF8A71', border: 'none', borderRadius: '3px', fontSize: 12, fontWeight: 600, }} />
 				  <Legend  />
-				  <Area dataKey="max" stroke="#8884d8" fill="#8884d8" />
+				  <Area dataKey="Recruiters" stroke="#8884d8" fill="#8884d8" />
 			  </AreaChart>
 		  </ResponsiveContainer>
 	  </div>
