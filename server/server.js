1f03879727abae6de57bba2073770ba10774b4d1
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-24 23:37:36 +0530

    in server.js  express and body parser initialized

--- server/server.js
+++ server/server.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+// EXPRESS INITIALIZATION
+const app = express()
+
+
+
+// APP CONFIGURATION
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+// DATABASE CONNECTION
+// import mongoseConnection from "./database/db.js";
+// mongoseConnection(); 
+
+
+
+
+
+// ROUTING
+// import router from './router/routes.js';
+// app.use('/', router)
+
+
+const PORT = 8000;
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`)
+})
