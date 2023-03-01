ec1dffddbf78bff3699febd3e85670a63aa2b127
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-02 02:03:27 +0530

    login if already have a valid JWT tokan

--- studentsLogin/src/LoginPage/Login.jsx
+++ studentsLogin/src/LoginPage/Login.jsx
@@ -1,4 +1,4 @@
-// import React, { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import './login.css';
 import logo from '../asset/KAHE_LOGO.png'
 import { LoginSocialGoogle } from 'reactjs-social-login';
@@ -7,18 +7,18 @@ import { useNavigate } from 'react-router-dom';
 import LoginButton from './LoginButton/LoginButton';
 
 const PORT = process.env.REACT_APP_SERVER_PORT;
-// const validateAccessTokan = async (aaccessToken) => {
-// 	try {
-// 		const responce = await axios.post("http://localhost:8000/validate-access-tokan", {
-// 			aaccessToken: aaccessToken
-// 		});
+const validateAccessTokan = async (aaccessToken) => {
+	try {
+		const responce = await axios.post("http://localhost:8000/validate-access-tokan", {
+			aaccessToken: aaccessToken
+		});
 
-// 		// console.log(responce.data);
-// 		return responce.data
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
+		console.log(responce.data);
+		return responce.data
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 
 const Login = (props) => {
@@ -26,20 +26,20 @@ const Login = (props) => {
 
 
 
-	// useEffect(() => {
-	// 	const aaccessToken = localStorage.getItem("accessToken");
-	// 	console.log("aaccessToken from localStorage ==>>>>>>>>", aaccessToken)
-	// 	const validateToken = async () => {
-	// 		if (aaccessToken) {
-	// 			const userData = await validateAccessTokan(aaccessToken)
-	// 			// console.log(userData)
-	// 			props.setUser(userData);
-	// 			// navigate('/home')
-	// 		}
-	// 	}
-	// 	validateToken();
-	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	// }, []);
+	useEffect(() => {
+		const aaccessToken = localStorage.getItem("accessToken");
+		console.log("aaccessToken from localStorage ==>>>>>>>>", aaccessToken)
+		const validateToken = async () => {
+			if (aaccessToken) {
+				const userData = await validateAccessTokan(aaccessToken)
+				console.log(userData)
+				props.setUser(userData);
+				navigate('/placement-drive')
+			}
+		}
+		validateToken();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 
 	const handleLoginResponce = async (data) => {
