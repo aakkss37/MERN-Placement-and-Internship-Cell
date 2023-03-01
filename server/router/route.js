a97150b5003f9619e21ba38ad9284fd3a4c33b11
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-02 02:02:24 +0530

    route set

--- server/router/route.js
+++ server/router/route.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { alreadyLogedIn } from "../controllers/alreadyLogedIn.js";
+import { JWTvalidation } from "../controllers/JWT-validation.js";
 import { loginStudent } from "../controllers/login.js";
 const router = express.Router();
 
 
 router.post('/login', loginStudent)
-
+router.post('/validate-access-tokan', JWTvalidation, alreadyLogedIn)
 
 
 export default router
