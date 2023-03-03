2be7642f853386facb1d2e6bd0bd8550986e3f2f
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-03 15:35:18 +0530

    responce send optimised

--- server/controllers/alreadyLogedIn.js
+++ server/controllers/alreadyLogedIn.js
@@ -5,15 +5,8 @@ export const alreadyLogedIn = async(req, resp)=> {
 	// console.log(req.student)
 	try {
 		const student = await Student.findOne({email: req.student.email});
-		// console.log(student)
-		return resp.status(200).json({
-			name: student.name,
-			email: student.email,
-			picture: student.picture,
-			cgpa: student.cgpa,
-			activeBack: student.activeBack,
-			phone: student.phone,
-		})
+		console.log("already logedin --->>>>>>>>>> ",student)
+		return resp.status(200).json(student)
 	} catch (error) {
 		return resp.status(404).json({ msg: error.message });
 	}
