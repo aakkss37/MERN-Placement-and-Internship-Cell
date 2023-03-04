d8507902fa90ddff3619665e5ed0607e0d19ee26
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-04 21:29:10 +0530

    Login route

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, } from 'react-router-dom';
 
 import './App.css'
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Navbar from './components/Navigation/Navbar'
 
 
+const PrivateRoute = (props) => {
+	return (
+		props.isLogin
+			?
+			<>
+				<Navbar />
+				<Outlet />
+			</>
+			:
+			<Login />
+	)
+}
+
 const App = () => {
-  return (
-	<BrowserRouter>
-		<Navbar/>
-		<Routes>
-			<Route path='/' element={<Home/>} />
-		</Routes>
-	</BrowserRouter>
-  )
+	// const [isLogin, setisLogin] = useState(true);
+	const isLogin = true
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Login />} />
+
+				<Route path='/home' element={<PrivateRoute isLogin={isLogin} />}>
+					<Route path='/home' element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
