af624959318f524b1c26fb002fbab78146f0e6a3
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-05 01:18:52 +0530

    name changed

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Outlet, } from 'react-router-dom';
-
-import './App.css'
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Navbar from './components/Navigation/Navbar'
