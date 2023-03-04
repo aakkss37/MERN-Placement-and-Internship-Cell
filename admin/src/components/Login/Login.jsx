90f15ff154aa954a6d0d4fec06b8a0bb84554f10
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-04 22:16:20 +0530

    login page ready

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './logo.css'
+import logo from '../../assets/KAHE_LOGO.png';
 
 const Login = () => {
-  return (
-	<div>Login</div>
-  )
+	return (
+		<div className='login'>
+			<div className='login_container'>
+				<div className='login_college_logo'>
+					<img src={logo} alt="img" />
+				</div>
+				<div className='kahe_login_text'>
+					<h3>Karpagan Academy of Higher Education</h3>
+					<p>Placement and Internship Cell</p>
+				</div>
+				<div className='login_form_container'>
+					<input type="text" placeholder='Username'/>
+					<input type="password" placeholder='Password'/>
+					<button>Login</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Login
