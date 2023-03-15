e39e98c6c876a2f6e6e225bdbc302dc17d265956
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-15 18:16:31 +0530

    company per year graph constructed

--- admin/src/components/PlacementData/PlacementData.jsx
+++ admin/src/components/PlacementData/PlacementData.jsx
@@ -4,6 +4,7 @@ import PlacementPerYear from './Statistics/PlacementPerYear';
 import './placementData.css'
 import { CurretPath } from './placementDataStyle'
 import { Divider } from '@mui/material';
+import CompanyPerYear from './Statistics/CompanyPerYear';
 
 // console.log(data, departments)
 const PlacementData = () => {
@@ -17,6 +18,8 @@ const PlacementData = () => {
 				<DepartmentWise/>
 				<Divider/>
 				<PlacementPerYear/>
+				<Divider/>
+				<CompanyPerYear/>
 			</div>
 		</div>
 	)
---
+++ admin/src/components/PlacementData/Statistics/CompanyPerYear.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2';
+
+const CompanyPerYear = () => {
+	const chartData = {
+		labels: ["2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+		datasets: [{
+			label: `Number of Company Visited in each Year`,
+			fill: false,
+			data: [121, 132, 183, 221, 168, 288, 341, 411, 517, 311, 382],
+			backgroundColor: 'rgba(54, 162, 235, 0.2)',
+			borderColor: 'rgba(54, 162, 235, 1)',
+			borderWidth: 1
+		}]
+	};
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Bar Chart'
+			}
+		}
+	};
+
+	return (
+		<div className="placement_data_graphs">
+			<h2>Company Visited Per-Year </h2> <br />
+			<Line type='line' data={chartData} options={options} />
+		</div>
+	)
+}
+
+export default CompanyPerYear
