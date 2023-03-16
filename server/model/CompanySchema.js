52052ef60f8f6b531b1dfdc77873e648b5986e95
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-16 23:54:46 +0530

    company schema created

---
+++ server/model/CompanySchema.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+
+const companySchema = new mongoose.Schema({
+	companyName: {
+		type: String,
+		required: true
+	},
+	cgpa: {
+		type: String,
+		required: true
+	},
+	activeBack: {
+		type: Boolean,
+		required: true,
+	},
+	passoutYear: {
+		type: String,
+	},
+	jobType: {
+		type: String,
+		required: true,
+	},
+	jobRole: {
+		type: String,
+		required: true,
+	},
+	
+	CTC: {
+		type: String,
+		required: true,
+	},
+	jobLocation: {
+		type: String,
+		required: true,
+	},
+	responsibilities: {
+		type: String,
+		required: true,
+	},
+	requirement: {
+		type: String,
+		required: true,
+	},
+	aboutCompany: {
+		type: String,
+		required: true,
+	},
+	companyLogo: {
+		type: String,
+		required: true,
+	},
+	
+	department: {
+		type: Array,
+		required: true,
+	},
+	
+})
+
+const Company = mongoose.model('Company', companySchema);
+
+export default Company;
