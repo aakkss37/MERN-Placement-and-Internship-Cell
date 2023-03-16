59ad06f38fc46525c8cd209347dfd99e5251423a
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-17 00:25:19 +0530

    company controller woring and saved data to database

--- server/controllers/adminController/companyController.js
+++ server/controllers/adminController/companyController.js
@@ -1,6 +1,12 @@
+import Company from "../../model/CompanySchema.js";
 
-
-export const addNewCompany = (request, response) => {
-	console.log(request.body)
-	response.status(200).json({ msg: "sucessfull receiver"});
+export const addNewCompany = async(request, response) => {
+	// console.log(request.body)
+	try {
+		const company = await Company.create(request.body)
+		console.log(company)
+		response.status(200).json({ msg: "New company added sucessfully."});
+	} catch (error) {
+		response.status(500).json({ msg: "Error while adding new company."});
+	}
 }
