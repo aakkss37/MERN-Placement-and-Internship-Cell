7575d30d5c3ef19be8e1b16567098ea552d53df4
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-16 23:10:04 +0530

    comment out console.log

--- server/controllers/studentController/alreadyLogedIn.js
+++ server/controllers/studentController/alreadyLogedIn.js
@@ -5,7 +5,7 @@ export const alreadyLogedIn = async(req, resp)=> {
 	// console.log(req.student)
 	try {
 		const student = await Student.findOne({email: req.student.email});
-		console.log("already logedin --->>>>>>>>>> ",student)
+		// console.log("already logedin --->>>>>>>>>> ",student)
 		return resp.status(200).json(student)
 	} catch (error) {
 		return resp.status(404).json({ msg: error.message });
