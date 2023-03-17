37dc85ddda4924c90f1da2186663f3c1bdc582a3
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-17 13:23:20 +0530

    get company list route set

--- server/router/route.js
+++ server/router/route.js
@@ -4,7 +4,7 @@ import { JWTvalidation } from "../controllers/studentController/JWT-validation.j
 import { loginStudent } from "../controllers/studentController/login.js";
 import { getImage, uploadImage } from "../controllers/adminController/companyLogoController.js";
 import upload from '../utils/UploadFile.js';
-import { addNewCompany } from "../controllers/adminController/companyController.js";
+import { addNewCompany, getListedCompanyList } from "../controllers/adminController/companyController.js";
 const router = express.Router();
 
 // STUDENT LOGIN
@@ -17,4 +17,5 @@ router.post('/validate-access-tokan', JWTvalidation, alreadyLogedIn)
 router.post('/upload-company-logo', upload.single('file'), uploadImage);
 router.get('/file/:filename', getImage);
 router.post('/add-new-company', addNewCompany)
+router.get('/get-company-list', getListedCompanyList)
 export default router
