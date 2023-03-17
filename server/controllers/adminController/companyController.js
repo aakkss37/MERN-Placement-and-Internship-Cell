25baa5d91923f93b9780e4b6ee0536e3fea9a86e
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-17 13:49:44 +0530

    required responce is ready for the request

--- server/controllers/adminController/companyController.js
+++ server/controllers/adminController/companyController.js
@@ -1,22 +1,33 @@
 import Company from "../../model/CompanySchema.js";
 
-export const addNewCompany = async(request, response) => {
+export const addNewCompany = async (request, response) => {
 	// console.log(request.body)
 	try {
 		const company = await Company.create(request.body)
-		// console.log(company)
-		response.status(200).json({ msg: "New company added sucessfully.", info: company});
+		console.log(company)
+		const infoToSend = {
+			id: company._id,
+			companyName: company.companyName,
+			listingDate: company.Date
+		}
+		response.status(200).json({ msg: "New company added sucessfully.", info: infoToSend });
 	} catch (error) {
-		response.status(500).json({ msg: "Error while adding new company."});
+		response.status(500).json({ msg: "Error while adding new company." });
 	}
 }
 
 
-export const getListedCompanyList = async(request, responce) => {
+export const getListedCompanyList = async (request, response) => {
 	try {
 		const list = await Company.find();
 		console.log(list)
+		const infoToSend = list.map(item => ({
+			id: item._id,
+			companyName: item.companyName,
+			listingDate: item.Date
+		}))
+		response.status(200).json({ info: infoToSend });
 	} catch (error) {
-		responce.status(500).json({msg: "Something went wrong while fetching the company list"})
+		response.status(500).json({ msg: error.message })
 	}
 }
