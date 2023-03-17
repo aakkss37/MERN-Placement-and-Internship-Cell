0155022a33367688e58ddb6d238c37c67cd43a09
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-17 13:21:15 +0530

    controller built for getting company list

--- server/controllers/adminController/companyController.js
+++ server/controllers/adminController/companyController.js
@@ -4,9 +4,19 @@ export const addNewCompany = async(request, response) => {
 	// console.log(request.body)
 	try {
 		const company = await Company.create(request.body)
-		console.log(company)
+		// console.log(company)
 		response.status(200).json({ msg: "New company added sucessfully.", info: company});
 	} catch (error) {
 		response.status(500).json({ msg: "Error while adding new company."});
+	}
+}
+
+
+export const getListedCompanyList = async(request, responce) => {
+	try {
+		const list = await Company.find();
+		console.log(list)
+	} catch (error) {
+		responce.status(500).json({msg: "Something went wrong while fetching the company list"})
 	}
 }
