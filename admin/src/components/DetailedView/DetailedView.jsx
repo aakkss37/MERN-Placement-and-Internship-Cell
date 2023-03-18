71f087f08981bc65eb859536a9825829ef6c276f
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-18 12:55:33 +0530

    styled and css

--- admin/src/components/DetailedView/DetailedView.jsx
+++ admin/src/components/DetailedView/DetailedView.jsx
@@ -1,24 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { API } from '../../services/api';
+import './detailedView.css'
+import { CurretPath } from './detailedViewStyle';
 
 const DetailedView = () => {
+	const [companyDetail, setCompanyDetail] = useState();
 	const [searchParams] = useSearchParams();
 	const companyId = searchParams.get('companyid');
 	// console.log(companyId)
 	useEffect(() => {
 		const getDetails = async () => {
 			try {
-				const companyDetail = await API.getCompanyDetails(companyId)
-				console.log(companyDetail.data)
+				const detail = await API.getCompanyDetails(companyId)
+				console.log(detail.data)
+				setCompanyDetail(detail.data)
 			} catch (error) {
 				console.log(error.message)
 			}
 		}
 		getDetails();
 	}, [companyId])
+
+
+
+
   return (
-	<div>DetailedView</div>
+	<div className='detailed_view'>
+		<div className='detailed_view_container'>
+			  <CurretPath >Home &gt; Company Details</CurretPath>
+		</div>
+	</div>
   )
 }
 
