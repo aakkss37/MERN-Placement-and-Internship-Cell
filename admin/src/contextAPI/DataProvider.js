dcfcb399fe2beb40f84e375253333fe163285ffb
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-19 19:34:53 +0530

    state managed what to show (applicant || test || interwiew) list

--- admin/src/contextAPI/DataProvider.js
+++ admin/src/contextAPI/DataProvider.js
@@ -9,11 +9,17 @@ export const DataContext = createContext(null);
 const DataProvider = (props) => {
 	const [studentsForTest, setStudentsForTest] = useState([])
 	const [studentsForInterview, setStudentsForInterview] = useState([])
+	const [showApplicantList, setShowApplicantList] = useState(false)
+	const [showTestList, setShowTestList] = useState(false)
+	const [showInterviewList, setShowInterviewList] = useState(false)
 
 	return (
 		<DataContext.Provider value={{
 			studentsForTest, setStudentsForTest,
-			studentsForInterview, setStudentsForInterview
+			studentsForInterview, setStudentsForInterview,
+			showApplicantList, setShowApplicantList,
+			showTestList, setShowTestList,
+			showInterviewList, setShowInterviewList,
 		}}>
 			{props.children}
 		</DataContext.Provider>
