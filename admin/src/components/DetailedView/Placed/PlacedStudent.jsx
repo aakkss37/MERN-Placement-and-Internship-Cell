702fbeb202f4e5deeb5d7f1c658de34466cd5cf4
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-19 20:24:47 +0530

    student placed completed

--- admin/src/components/DetailedView/Placed/PlacedStudent.jsx
+++ admin/src/components/DetailedView/Placed/PlacedStudent.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../../../contextAPI/DataProvider';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,53 +7,48 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
 
 const PlacedStudent = () => {
+	const context = useContext(DataContext);
 
+	const rows = context.studentsPlaced
+	console.log(context.studentsPlaced)
 
-	function createData (name, calories, fat, carbs, protein) {
-		return { name, calories, fat, carbs, protein };
-	}
-
-	const rows = [
-		createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-		createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-		createData('Eclair', 262, 16.0, 24, 6.0),
-		createData('Cupcake', 305, 3.7, 67, 4.3),
-		createData('Gingerbread', 356, 16.0, 49, 3.9),
-	];
-
-  return (
-	  <TableContainer component={Paper}>
-		  <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-			  <TableHead>
-				  <TableRow>
-					  <TableCell>Dessert (100g serving)</TableCell>
-					  <TableCell align="right">Calories</TableCell>
-					  <TableCell align="right">Fat&nbsp;(g)</TableCell>
-					  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-					  <TableCell align="right">Protein&nbsp;(g)</TableCell>
-				  </TableRow>
-			  </TableHead>
-			  <TableBody>
-				  {rows.map((row) => (
-					  <TableRow
-						  key={row.name}
-						  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-					  >
-						  <TableCell component="th" scope="row">
-							  {row.name}
-						  </TableCell>
-						  <TableCell align="right">{row.calories}</TableCell>
-						  <TableCell align="right">{row.fat}</TableCell>
-						  <TableCell align="right">{row.carbs}</TableCell>
-						  <TableCell align="right">{row.protein}</TableCell>
-					  </TableRow>
-				  ))}
-			  </TableBody>
-		  </Table>
-	  </TableContainer>
-  )
+	return (
+		<>
+			<Typography sx={{ fontSize: '26px', margin: "20px", marginLeft: '5%', fontWeight: 600, color: '#1976d2' }}>Placed Student</Typography>
+			<TableContainer component={Paper} sx={{ height: 300, width: '90%', marginLeft: '5%', marginBottom: '2rem'}}>
+				<Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+					<TableHead>
+						<TableRow>
+							<TableCell >Roll No.</TableCell>
+							<TableCell >Name</TableCell>
+							<TableCell align="right">Department</TableCell>
+							<TableCell align="right">CGPA</TableCell>
+							<TableCell align="right">Phone</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{rows.map((row) => (
+							<TableRow
+								key={row.id}
+								sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+							>
+								<TableCell component="th" scope="row">
+									{row.id}
+								</TableCell>
+								<TableCell >{row.Name}</TableCell>
+								<TableCell align="right">{row.Department}</TableCell>
+								<TableCell align="right">{row.CGPA}</TableCell>
+								<TableCell align="right">{row.phone}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
+	)
 }
 
 export default PlacedStudent
