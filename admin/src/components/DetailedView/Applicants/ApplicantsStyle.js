3436ec63d28e783727c8caf9b3b4f6081c68ecd4
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-19 14:27:43 +0530

    applicant search styled

--- admin/src/components/DetailedView/Applicants/ApplicantsStyle.js
+++ admin/src/components/DetailedView/Applicants/ApplicantsStyle.js
@@ -1,3 +1,4 @@
+import { InputBase } from '@mui/material';
 import { styled, alpha } from '@mui/material/styles';
 
 export const Search = styled('div')(({ theme }) => ({
@@ -23,4 +24,21 @@ export const SearchIconWrapper = styled('div')(({ theme }) => ({
 	display: 'flex',
 	alignItems: 'center',
 	justifyContent: 'center',
+}));
+
+export const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+				width: '20ch',
+			},
+		},
+	},
 }));
