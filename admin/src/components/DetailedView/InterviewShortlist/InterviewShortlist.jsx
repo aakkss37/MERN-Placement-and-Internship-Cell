e9841dc95887139fc16c0e9c45e5f899c66ecc0f
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-19 20:00:07 +0530

    creating interwiew shortlist

---
+++ admin/src/components/DetailedView/InterviewShortlist/InterviewShortlist.jsx
@@ -0,0 +1,106 @@
+import React, { useContext, useState } from 'react';
+import { DataContext } from '../../../contextAPI/DataProvider';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { Button, } from '@mui/material';
+import { ApplicantsAction, Search, SearchIconWrapper, StyledInputBase } from './InterviewShortlistStyle';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const columns = [
+	{
+		field: 'id',
+		headerName: 'Roll no.',
+		width: 150
+	},
+	{
+		field: 'Name',
+		headerName: 'Name',
+		width: 150,
+	},
+	{
+		field: 'Department',
+		headerName: 'Department',
+		width: 200,
+	},
+	{
+		field: 'CGPA',
+		headerName: 'CGPA',
+		width: 90,
+	},
+	{
+		field: 'phone',
+		headerName: 'Phone',
+		width: 160,
+	},
+];
+
+
+
+const TestShortlist = ({ driveData }) => {
+	const [searchText, setSearchText] = useState('');
+	const [studentsChecked, setStudentsChecked] = useState([]);
+	const context = useContext(DataContext);
+
+
+	const handleSearch = (event) => {
+		setSearchText(event.target.value);
+	};
+	const rows = context.studentsForTest
+	const filteredRows = rows.filter((row) =>
+		row.id.toString().startsWith(searchText)
+	);
+
+
+	const handleCheck = (event) => {
+		setStudentsChecked([...studentsChecked, event.row])
+	}
+	const selectedForInterview = () => {
+		context.setStudentsForInterview(studentsChecked)
+		context.setShowInterviewList(true)
+	}
+	// console.log(context.studentsForInterview)
+	// console.log(studentsChecked)
+	return (
+		<Box sx={{ height: 400, width: '90%', marginLeft: '5%' }}>
+			<ApplicantsAction>
+				<Search>
+					<SearchIconWrapper>
+						<SearchIcon />
+					</SearchIconWrapper>
+					<StyledInputBase
+						placeholder="Search Roll no."
+						inputProps={{ 'aria-label': 'search' }}
+						value={searchText}
+						onChange={handleSearch}
+					/>
+				</Search>
+				<h2 style={{ fontSize: '1.5rem', }}>Short list for Interview</h2>
+				{
+					studentsChecked.length ?
+						<Button variant="contained" onClick={selectedForInterview}>Select For Interview</Button>
+						:
+						<Button variant="contained" disabled>Select For Interview</Button>
+				}
+			</ApplicantsAction>
+
+			<DataGrid
+				rows={filteredRows}
+				columns={columns}
+				initialState={{
+					pagination: {
+						paginationModel: {
+							pageSize: 10,
+						},
+					},
+				}}
+				pageSizeOptions={[10]}
+				checkboxSelection
+				disableRowSelectionOnClick={true}
+				onCellClick={(event) => { handleCheck(event) }}
+			/>
+		</Box>
+	)
+}
+
+export default TestShortlist
