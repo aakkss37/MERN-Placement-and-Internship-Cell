6f3ff53bb3b2abf8205cf3f9ea3478a31c271d51
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-21 07:07:42 +0530

    delete show state"

--- admin/src/contextAPI/DataProvider.js
+++ admin/src/contextAPI/DataProvider.js
@@ -10,20 +10,13 @@ const DataProvider = (props) => {
 	const [studentsForTest, setStudentsForTest] = useState([])
 	const [studentsForInterview, setStudentsForInterview] = useState([])
 	const [studentsPlaced, setStudentsPlaced] = useState([])
-	const [showApplicantList, setShowApplicantList] = useState(false)
-	const [showTestList, setShowTestList] = useState(false)
-	const [showInterviewList, setShowInterviewList] = useState(false)
-	const [showPlacementList, setShowPlacementList] = useState(false)
+
 
 	return (
 		<DataContext.Provider value={{
 			studentsForTest, setStudentsForTest,
 			studentsForInterview, setStudentsForInterview,
-			showApplicantList, setShowApplicantList,
-			showTestList, setShowTestList,
-			showInterviewList, setShowInterviewList,
 			studentsPlaced, setStudentsPlaced,
-			showPlacementList, setShowPlacementList,
 		}}>
 			{props.children}
 		</DataContext.Provider>
