c8622f6aece912fd88d30bef9a90445173dc9d19
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-22 20:53:50 +0530

    new file for sucess and error msg

---
+++ admin/src/components/ui/ErrorMsg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorMsg = () => {
+  return (
+	<div>ErrorMsg</div>
+  )
+}
+
+export default ErrorMsg
---
+++ admin/src/components/ui/SuccessMsg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuccessMsg = () => {
+  return (
+	<div>SnackBar</div>
+  )
+}
+
+export default SuccessMsg
