ede19fc690fbf4cfd374d32f035a4c2520399e45
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-23 12:34:34 +0530

    setting menue added

--- admin/src/components/Navigation/Navbar.jsx
+++ admin/src/components/Navigation/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import './navbar.css'
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Menu, MenuItem } from '@mui/material';
 
 
 const Navbar = () => {
 	const navigate = useNavigate()
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 	return (
 		<div className='navbar'>
 			<div className='navbar_container' >
@@ -13,13 +23,39 @@ const Navbar = () => {
 					<img src={logo} alt='logo' />
 				</div>
 				<div className='navbar_links'>
-					<span onClick={()=> navigate('/home')}>Home</span>
+					<span onClick={() => navigate('/home')}>Home</span>
 					<span onClick={() => navigate('/new-company')}>New Company</span>
 					<span onClick={() => navigate('/placement-data')}>Placement Data</span>
 					<span onClick={() => navigate('/home')}>Students</span>
 				</div>
 				<div className='navbar_logout'>
 					<span>Logout</span>
+				</div>
+				<div>
+					<button
+						className='setting_icon_button'
+						id="basic-button"
+						aria-controls={open ? 'basic-menu' : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? 'true' : undefined}
+						onClick={handleClick}
+					>
+						<SettingsIcon sx={{ color: 'white' }} />
+					</button>
+					<Menu
+						id="basic-menu"
+						anchorEl={anchorEl}
+						open={open}
+						onClose={handleClose}
+						MenuListProps={{
+							'aria-labelledby': 'basic-button',
+						}}
+					>
+						<MenuItem onClick={handleClose} >Profile</MenuItem>
+						<MenuItem onClick={handleClose} >My account</MenuItem>
+						<MenuItem onClick={handleClose} >Logout</MenuItem>
+						<MenuItem onClick={handleClose} >Change Password</MenuItem>
+					</Menu>
 				</div>
 			</div>
 		</div>
