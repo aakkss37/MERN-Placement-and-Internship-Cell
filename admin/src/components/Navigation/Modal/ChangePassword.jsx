cf5af46646c5ef0c00f2f7cfa8f4d26ba0ff9a21
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-23 13:00:47 +0530

    model component is ready to use

--- admin/src/components/Navigation/Modal/ChangePassword.jsx
+++ admin/src/components/Navigation/Modal/ChangePassword.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Modal, Typography } from '@mui/material';
+import { Box, Button, Modal, TextField, Typography } from '@mui/material';
 
 
 
@@ -11,30 +11,66 @@ const style = {
 	width: 400,
 	bgcolor: 'background.paper',
 	boxShadow: 20,
+	borderRadius: '5px',
 	p: 4,
 };
 
 
 const ChangePassword = (props) => {
-  return (
-	  <div>
-		  <Modal
-			  open={props.open}
-			  onClose={props.onClose}
-			  aria-labelledby="modal-modal-title"
-			  aria-describedby="modal-modal-description"
-		  >
-			  <Box sx={style}>
-				  <Typography id="modal-modal-title" variant="h6" component="h2">
-					  Text in a modal
-				  </Typography>
-				  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-					  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-				  </Typography>
-			  </Box>
-		  </Modal>
-	  </div>
-  )
+
+	const changePasswortHandler = () => { }
+	return (
+		<div>
+			<Modal
+				open={props.open}
+				onClose={props.onClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>
+					<Typography component="h5" variant="h5" color="primary" style={{ fontWeight: 600, color: "gray", textAlign: "center" }}>
+						Change Password
+					</Typography>
+					<form  >
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="currentPassword"
+							label="Current Password"
+							name="currentPassword"
+							autoComplete="currentPassword"
+							autoFocus
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="newPassword"
+							label="New Password"
+							type="newPassword"
+							id="newPassword"
+							autoComplete="newPassword"
+						/>
+
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							sx={{ marginTop: '1rem' }}
+							onClick={changePasswortHandler}
+						>
+							Change Password
+						</Button>
+
+					</form>
+				</Box>
+			</Modal>
+		</div>
+	)
 }
 
 export default ChangePassword
