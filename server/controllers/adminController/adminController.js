6151b7047fb914cefec0567d4de7b53607c1cf4b
amar kumar <amarkumar.sharma.124@gmail.com>
2023-03-23 13:53:31 +0530

    create admin controller completed

---
+++ server/controllers/adminController/adminController.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcrypt';
+import Admin from '../../model/adminSchema';
+
+
+export const createAdmin = async (request, responce) => {
+	console.log("create admin: ---> ", request.body)
+	try {
+		const encryptedPassword = await bcrypt.hash(request.body.password, 10); // --> bcrypt(password, salt_value)
+		const adminCredentials = {
+			username: request.body.username,
+			password: encryptedPassword
+		}
+		// console.log("serverGenerated: ---> ", adminCredentials);
+		const admin = await Admin.create(adminCredentials);
+		await admin.save();
+		return responce.status(200).json({ msg: 'signup sucessfull' });
+	} catch (error) {
+		return responce.status(500).json({ msg: 'Error while signing up...' });
+	}
+}
